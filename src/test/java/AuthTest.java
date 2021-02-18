82b1f5ce35992af98479b430584ca016d2dab04f
Anastasia <n41111215@mail.ru>
2021-02-18 16:37:05 +0300

    add SadPath

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.netology.web.DataGenerator;
 import ru.netology.web.User;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.web.DataGenerator.Registration.generateValidUser;
+import static ru.netology.web.DataGenerator.Registration.generateBlockedUser;
 
 public class AuthTest {
     @BeforeEach
@@ -16,11 +17,23 @@ public class AuthTest {
 
     @Test
     void shouldSendFormValid() {
-        User validUser = generateValidUser();
+        User validUser = DataGenerator.Registration.generateValidUser();
         $("[data-test-id=login] input").setValue(validUser.getLogin());
         $("[data-test-id=password] input").setValue(validUser.getPassword());
         $("button[data-test-id=action-login]").click();
-        $(".App_appContainer__3jRx1 h2.heading").shouldBe(visible).shouldHave(text("Личный кабинет"));
+        $(".App_appContainer__3jRx1").shouldBe(visible).shouldHave(text("Личный кабинет"));
     }
+
+    @Test
+    void shouldSendFormBlockedUser() {
+        User blockedUser = generateBlockedUser();
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("button[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(visible).shouldHave(text("Ошибка"));
+    }
+
+
 }
+
 
